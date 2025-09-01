56385974ccaed311cead0184060761528ef62155
Daniel Bauer <18196250+dan13bauer@users.noreply.github.com>
2025-09-01 10:49:10 +0200

    Add cuDF parquet reader support in PlanBuilder.cpp and ToCudf.cpp

--- velox/experimental/cudf/exec/ToCudf.h
+++ velox/experimental/cudf/exec/ToCudf.h
@@ -107,6 +107,14 @@ class CudfOptions {
     return transformLastOutput_;
   }
 
+  void setParquetConnectorRegistered(bool newValue) {
+    parquetConnectorRegistered_ = newValue;
+  }
+
+  const bool isParquetConnectorRegistered() const {
+    return parquetConnectorRegistered_;
+  }
+
   const bool cudfEnabled;
   const std::string cudfMemoryResource;
   const bool cudfTableScan;
@@ -123,10 +131,12 @@ class CudfOptions {
         cudfExchange(FLAGS_velox_cudf_exchange),
         memoryPercent(50),
         prefix_(""),
+        parquetConnectorRegistered_(false),
         transformLastOutput_(false) {}
   CudfOptions(const CudfOptions&) = delete;
   CudfOptions& operator=(const CudfOptions&) = delete;
   std::string prefix_;
+  bool parquetConnectorRegistered_;
   bool transformLastOutput_;
 };
 
@@ -143,8 +153,9 @@ bool cudfIsRegistered();
 bool cudfDebugEnabled();
 
 /**
- * @brief Returns true if the velox_cudf_table_scan flag is set to true.
+ * @brief Returns true if an application indicated that the parquet connector is
+ * registered
  */
-bool cudfTableScanEnabled();
+bool isParquetConnectorRegistered();
 
 } // namespace facebook::velox::cudf_velox
